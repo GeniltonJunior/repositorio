aee17fa51affbf90ef5d4e7a79b9f7820a892889
Genilton Junior <165211700+GeniltonJunior@users.noreply.github.com>
2024-04-01 20:10:12 -0300

    Update aula07a.py

--- Curso em video/Aulas python/aula07a.py
+++ Curso em video/Aulas python/aula07a.py
@@ -1,2 +1,9 @@
-nome = input('Qual o seu nome? ')
-print('Prazer em te conhecer {}!'.format(nome))
+n1= int(input('Um valor: '))
+n2= int(input('Outro valor: '))
+s = n1+n2
+m = n1*n2
+d = n1/n2
+di = n1//n2
+e = n1**n2
+print('A soma é {}, \n o produto é {} e a \n divisão é {:.3f}'.format(s,m,d), end=' ')
+print('Divisão inteira {} e Potencia {} !'.format(di,e))
