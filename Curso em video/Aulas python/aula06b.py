f7b7a62e5cf65c90aa877978922477fbd1f37d85
Genilton Junior <165211700+GeniltonJunior@users.noreply.github.com>
2024-04-01 19:35:02 -0300

    nova adição

---
+++ Curso em video/Aulas python/aula06b.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um numero aqui '))
+n2 = int(input('Digite mais um numero '))
+n3 = int(input('Digite mais um numero '))
+n4 = int(input('digite o ultimo numero '))
+s = n1 + n2 + n3 + n4
+print('a soma dos numeros {0},{1},{2} e {3} vale {4}'.format(n1, n2, n3, n4, s))
