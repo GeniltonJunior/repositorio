3a57d07fb7fac74cc3560c86f7249cf4035846e7
Genilton Junior <165211700+GeniltonJunior@users.noreply.github.com>
2024-04-16 16:03:09 -0300

    Update ex007.py

--- Curso em video/desafios python/ex007.py
+++ Curso em video/desafios python/ex007.py
@@ -1,4 +1,4 @@
 n1 = float(input('Primeira nota do aluno: '))
 n2 = float(input('Segunda nota do aluno: ' ))
 m = (n1+n2)/2
-print('A média entre {} e {} é igual a {:.1f}'.format(n1,n2,m))
+print('A media entre {} e {} é igual a {:.1f}'.format(n1,n2,m))
