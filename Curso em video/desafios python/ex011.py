96ad56dedac075b391056a098a42fbaa6594b88b
Genilton Junior <165211700+GeniltonJunior@users.noreply.github.com>
2024-04-16 21:51:47 -0300

    Update ex011.py

---
+++ Curso em video/desafios python/ex011.py
@@ -0,0 +1,5 @@
+larg = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
+area = larg * alt
+tinta = area / 2
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m².\nPara pintar essa parede, você precisará de {}l de tinta.'.format(larg,alt,area,tinta))
