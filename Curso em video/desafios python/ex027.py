2dec95db80baaa2c681eb32f36fc97776d4f8851
Genilton Junior <165211700+GeniltonJunior@users.noreply.github.com>
2024-04-22 22:01:24 -0300

    ex 27

---
+++ Curso em video/desafios python/ex027.py
@@ -0,0 +1,4 @@
+nome = str(input('Digite seu nome completo: ')).strip().split()
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu Ultimo nome é {}'.format(nome[-1]))
